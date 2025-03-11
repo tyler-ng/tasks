aeb07e9e0c4efe96ff0448752b76d196d1fd05a1
Tyler Nguyen <tylernn812@gmail.com>
2025-03-11 00:17:06 -0700

    test pipe line 4

--- services/tasks_api/tests.py
+++ services/tasks_api/tests.py
@@ -18,4 +18,4 @@ def test_health_check(client):
     """
     response = client.get("/api/health-check/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() != {"message": "OK"}
+    assert response.json() == {"message": "OK"}
