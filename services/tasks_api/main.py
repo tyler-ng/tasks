abb88c300d2ac2591a4dbc001c41ca54a467378b
Tyler Nguyen <tylernn812@gmail.com>
2025-03-12 22:02:44 -0700

    Add deployment configuration

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 
 app = FastAPI()
 app.add_middleware(
@@ -14,3 +15,5 @@ app.add_middleware(
 @app.get("/api/health-check/")
 def health_check():
     return {"message": "OK"}
+
+handle = Mangum(app)
